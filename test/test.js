88415317fb0558d757f5ad50ddf43848e4667ef2
Student-na-praksi <fg1343@student.uni-lj.si>
2024-04-25 22:27:24 +0200

    Prepared test envirnoment and first test has passed

---
+++ test/test.js
@@ -0,0 +1,15 @@
+import request from 'supertest';
+
+describe('Home Page', () => {
+  it('User Authentication', (done) => {
+    request('http://localhost:5000') // replace with your server's URL and port
+      .get('/')
+      .expect(/<h2>User Authentication<\/h2>/i, done);
+  });
+
+//   it('Display paragraph "A veš, da ti verjetno ne veš ..."', (done) => {
+//     request('http://localhost:5000') // replace with your server's URL and port
+//       .get('/')
+//       .expect(/<p>A veš, da ti verjetno ne veš, da jaz vem, da ti uporabljaš naslednji spletni brskalnik\?<\/p>/i, done);
+//   });
+});
